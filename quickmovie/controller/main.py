a5b8bc05bad7198af118e7f015e551e0e285a86f
Yann Ramin <atrus@stackworks.net>
2008-06-01 00:12:50 -0700

    Updates with ajaxyness

--- quickmovie/controller/main.py
+++ quickmovie/controller/main.py
@@ -7,9 +7,15 @@ from quickmovie.model import Movie
 class Main(object):
 
     @cherrypy.expose
-    def index(self):
+    def plot(self, movie):
+        m = meta.Session.query(Movie).filter_by(id = movie)[0]
+        return m.plot
+
+    @cherrypy.expose
+    def index(self, images = True, plot = False):
         t = {}
         t['movies'] = meta.Session.query(Movie).order_by(Movie.name)
-
+        t['images'] = images
+        t['plot'] = plot
         meta.Session.commit()
         return template('index.html', t)
